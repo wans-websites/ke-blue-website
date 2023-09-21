b2a4b5934011e013ebaeb2a603b9fd64cd8962b6
Lum-ona <lumonamulengwa@gmail.com>
2023-09-21 23:05:04 +0300

    footer is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,40 +16,42 @@ export default function Header({ isMobileView }) {
   const location = useLocation().pathname;
 
   return (
-    <header className="header">
-      <div className="header-top">
-        <img className="logo" src={logo} alt="ke-blue logo" />
-        {isMobileView && (
-          <MenuIcon
-            className="menu-icon"
-            onClick={() =>
-              setIsNavMenuClicked((current) => (current = !current))
-            }
-          />
-        )}
-        <div className="info opening-hours">
-          <AccessTimeIcon className="top-header-icon" />
-          <div>
-            <p>Mon - Sat 9.00 - 18.00</p>
-            <p>Sunday Closed</p>
+    <>
+      <header className="header">
+        <div className="header-top">
+          <img className="logo" src={logo} alt="ke-blue logo" />
+          {isMobileView && (
+            <MenuIcon
+              className="menu-icon"
+              onClick={() =>
+                setIsNavMenuClicked((current) => (current = !current))
+              }
+            />
+          )}
+          <div className="info opening-hours">
+            <AccessTimeIcon className="top-header-icon" />
+            <div>
+              <p>Mon - Sat 9.00 - 18.00</p>
+              <p>Sunday Closed</p>
+            </div>
+          </div>
+          <div className="info email">
+            <MailOutlineIcon className="top-header-icon" />
+            <div>
+              <p>Email</p>
+              <p>info@ke-blue.com</p>
+            </div>
+          </div>
+          <div className="info call">
+            <CallIcon className="top-header-icon" />
+            <div>
+              <p>Call Us</p>
+              <p>+254-704-007-333</p>
+              <p>+254-704-576-165</p>
+            </div>
           </div>
         </div>
-        <div className="info email">
-          <MailOutlineIcon className="top-header-icon" />
-          <div>
-            <p>Email</p>
-            <p>info@ke-blue.com</p>
-          </div>
-        </div>
-        <div className="info call">
-          <CallIcon className="top-header-icon" />
-          <div>
-            <p>Call Us</p>
-            <p>+254-704-007-333</p>
-            <p>+254-704-576-165</p>
-          </div>
-        </div>
-      </div>
+      </header>
       <nav
         className={`nav-options ${isNavMenuClicked && "mobile-nav-options"} ${
           isMobileView && "mobile-view"
@@ -121,6 +123,6 @@ export default function Header({ isMobileView }) {
           Get In Touch
         </Link>
       </nav>
-    </header>
+    </>
   );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,7 +12,6 @@ export default function Home() {
       <Hero />
       <Whatwedo />
       <Whyus />
-      <HomeProduct />
       <Testimonial />
     </div>
   );
